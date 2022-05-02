36f8ab9b4aee43baa5a154b86c782a5cb70c9303
Nikolaevv2003 <92081840+Nikolaevv2003@users.noreply.github.com>
2022-05-03 01:13:25 +0300

    Update ConsoleApplication10.cpp

--- ConsoleApplication10/ConsoleApplication10/ConsoleApplication10.cpp
+++ ConsoleApplication10/ConsoleApplication10/ConsoleApplication10.cpp
@@ -4,19 +4,19 @@ using namespace std;
 int main()
 {
     int m, n;
-    char alphabet[30];
+    char s[30];
     m = 0;
 
     cout << "Enter the string: ";
-    cin >> alphabet;
+    cin >> s;
 
-    for (m; m < strlen(alphabet) - 1; m++) {
-        for (n = m; n < strlen(alphabet) - 1; n++) {
-            if (alphabet[m] > alphabet[n + 1])
+    for (m; m < strlen(s) - 1; m++) {
+        for (n = m; n < strlen(s) - 1; n++) {
+            if (s[m] > s[n + 1])
             {
-                swap(alphabet[m], alphabet[n + 1]);
+                swap(s[m], s[n + 1]);
             }
         }
     }
-    cout << alphabet;
+    cout << s;
 }
