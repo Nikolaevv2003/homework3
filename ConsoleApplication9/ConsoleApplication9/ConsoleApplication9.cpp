d7ec5634148ac0053b90c9700f98a28b8bdcb26b
Nikolaevv2003 <92081840+Nikolaevv2003@users.noreply.github.com>
2022-05-23 23:49:32 +0300

    Update ConsoleApplication9.cpp

--- ConsoleApplication9/ConsoleApplication9/ConsoleApplication9.cpp
+++ ConsoleApplication9/ConsoleApplication9/ConsoleApplication9.cpp
@@ -1,46 +1,39 @@
-﻿#include <iostream>
-#include <fstream>
-#include <sstream>
+#include <iostream>
+#include <cmath>
 #include <string>
-void output_what(std::ostream& _out, std::istream& _in, bool (*cmp)(int), const char* spl);
+#include <fstream>
+using namespace std;
 
-bool pred_num(int c) { return (c >= '0' && c <= '9'); }
+int main()
+{
+    string path = "file2.txt";
+    ofstream fout;
 
-int main(void) {
-    char s[] = "1-UML (200), 3, [2012], 97 AppleTalk-40";
-    std::istringstream sp(s);
-    //для примера ввод из строки
-    output_what(std::cout, sp, &pred_num, " ");
-    std::cout << std::endl;
+    int Number;
 
-    //создадим файл
-    char fn[] = "file.txt";
-    std::ofstream fout(fn);
-    fout << "1 _2_ 3 4 5 6 (7) 8 x9y Actor 777";
-    fout.close();
+    fout.open(path);
 
-    std::ifstream fin(fn);
-    //ввод из файла
-    output_what(std::cout, fin, &pred_num, " ");
-    fin.close();
-    std::cin.get();
-    return 0;
-}
 
-//ввод/вывод
-void output_what(std::ostream& _out, std::istream& _in, bool (*cmp)(int), const char* spl) {
-    std::string s;
-    std::istreambuf_iterator<char> i(_in), e;
-
-    for (; i != e; ++i) {
-        if ((*cmp)(*i))
-            s += *i;
-        else if (!s.empty()) {
-            _out << s << spl;
-            s = "";
-        }
+    if (!fout.is_open()) {
+        cout << "Error";
     }
 
-    if (!s.empty())
-        _out << s << spl;
+    else {
+        fout << 73463254 << "qwewrtydfsgvhj" << 462 << endl;
+    }
+
+    ifstream file(path);
+
+
+    do {
+        if (file >> Number) {
+            cout << Number;
+        }
+        else {
+            file.clear();
+            file.ignore(1, ' ');
+        }
+    } 
+    while (!file.eof());
+    fout.close();
 }
